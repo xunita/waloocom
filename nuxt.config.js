01785cb018372dc1714620b53c45a48045a0b0a4
sopegue <yayasopeguesoro@gmail.com>
2021-03-15 20:23:46 +0100

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,5 @@
 export default {
-  // target: 'static',
-  ssr: false,
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Waloo - More than an onlineshop',
